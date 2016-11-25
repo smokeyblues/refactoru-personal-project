3ec969bb13de83cf1a528cb193d3c3bebb47e9bd
smokeyblues <smokeyblues@hotmail.com>
2016-11-25 17:11:51 -0500

    finished a version of the lead-in animation

---
+++ main.js
@@ -0,0 +1,6 @@
+function bigQuestion () {
+    document.getElementById('middle').id='middle-animated';
+    document.getElementById('card1').id = 'card1-animate';
+    document.getElementById('card2').id = 'card2-animate';
+    document.getElementById('card3').id = 'card3-animate';
+}
