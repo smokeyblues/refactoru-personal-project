1bfb3917e4f03b5d5bd7b248720388c548226756
smokeyblues <smokeyblues@hotmail.com>
2016-11-27 14:17:05 -0500

    most of the reveal page animation is finished.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function bigQuestion () {
-    document.getElementById('middle').id='middle-animated';
-    document.getElementById('card1').id = 'card1-animate';
-    document.getElementById('card2').id = 'card2-animate';
-    document.getElementById('card3').id = 'card3-animate';
+        setInterval(function() { location.href="./fortune.html" }, 3000); 
+        document.getElementById('middle').id = 'middle-animated';
+        document.getElementById('card1').id = 'card1-animate';
+        document.getElementById('card2').id = 'card2-animate';
+        document.getElementById('card3').id = 'card3-animate';
 }
